428a836500d1cb7b980f36ae3daa36c9a7c3d7ac
_Kessler <101578713+Kessler007@users.noreply.github.com>
2022-04-18 16:25:02 +0200

    Add files via upload

---
+++ src/Nodos.java
@@ -0,0 +1,25 @@
+public class Nodos {
+    //dato
+    Object dato;
+    Nodos siguiente;
+    //metodo constructor
+    public Nodos (Object dato,Nodos siguiente) {
+        this. dato=dato;
+        this. siguiente= siguiente;
+    }
+
+    public Object getDato() {
+        return this.dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato=dato;
+    }
+    public Nodos getSiguiente(){
+        return this.siguiente;
+    }
+    public void setSiguiente(Nodos siguiente){
+        this.siguiente=siguiente;
+
+    }
+}
