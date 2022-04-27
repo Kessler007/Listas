6075813ef13695a185ab906fb6773f21a900e0d4
_Kessler <101578713+Kessler007@users.noreply.github.com>
2022-04-26 20:54:15 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+         * Lista_DE lista_DE=new Lista_DE();
+         * System.out.println(lista_DE.estaVacia());
+         * for (int i=20;i>0;i--){
+         * lista_DE.insertar(i);
+         * }
+         * System.out.println(lista_DE.estaVacia());
+         * lista_DE.mostrarAdelante();
+         * lista_DE.eliminar();
+         * lista_DE.eliminar();
+         * lista_DE.mostrarAdelante();
+         * lista_DE.mostrarAdelante();
+         */
+        /*
+         * Lista_CSE lista_cse=new Lista_CSE();
+         * System.out.println(lista_cse.estaVacia());
+         * for (int i=1;i<=12;i++){
+         * lista_cse.insertarFin(i);
+         * }
+         * lista_cse.mostrar();
+         * System.out.println(lista_cse.tamaño());
+         * //lista_cse.eliminarFin();
+         * //lista_cse.eliminarFin();
+         * //lista_cse.mostrar();
+         * lista_cse.mostraCircular(2);
+         */
+    }
+}
