6f91aac0c9943f3c876d5e9c410d9cc301cf94d3
_Kessler <101578713+Kessler007@users.noreply.github.com>
2022-05-09 16:07:28 +0200

    Main

--- Main.java
+++ Main.java
@@ -28,6 +28,17 @@ public class Main {
          * //lista_cse.mostrar();
          * lista_cse.mostraCircular(2);
          */
+             /*Lista_SE lista_se=new Lista_SE();
+
+        lista_se.Mostrar();
+
+        lista_se.InsetarInicio(999);
+        lista_se.InsetarInicio(45);
+        lista_se.InsetarInicio(567);
+        lista_se.InsetarInicio(45);
+
+        lista_se.Mostrar();*/
+        
         
         /*public static void main(String[] args) {
         Lista_circular_doblemente_enlazada lista_circular_doblemente_enlazada=new Lista_circular_doblemente_enlazada();
