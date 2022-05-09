bd2ea7ca45a6a17a9feae33a1797e656364b9000
_Kessler <101578713+Kessler007@users.noreply.github.com>
2022-05-09 15:59:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -28,5 +28,19 @@ public class Main {
          * //lista_cse.mostrar();
          * lista_cse.mostraCircular(2);
          */
+        
+        /*public static void main(String[] args) {
+        Lista_circular_doblemente_enlazada lista_circular_doblemente_enlazada=new Lista_circular_doblemente_enlazada();
+        System.out.println(lista_circular_doblemente_enlazada.estaVacio());
+        for (int i=1;i<=5;i++){
+            lista_circular_doblemente_enlazada.insertarFinal(i);
+        }
+        lista_circular_doblemente_enlazada.mostrar();
+        lista_circular_doblemente_enlazada.eliminarFinal();
+        lista_circular_doblemente_enlazada.eliminarFinal();
+        lista_circular_doblemente_enlazada.mostrar();
+        lista_circular_doblemente_enlazada.mostrarAdelante(8);
+        lista_circular_doblemente_enlazada.mostrarAdelante(8);*/
+    }
     }
 }
